bf5590a1bd05fc56f726c5c64c5b08a85fd6733e
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-23 22:30:40 -0700

    form change

--- script.mjs
+++ script.mjs
@@ -6,28 +6,31 @@ const numberOfRows = 5;
 const numberOfColumns = 5;
 const cardGrid = [];
 
+const gameBoard = document.querySelector("memoryGameBoard"); // Container for the cards.
+
 for (let i = 0; i < numberOfRows; i++) {
     const row = []; // Initializing an empty row.
     for (let j = 0; j < numberOfColumns; j++) {
-        const card = document.createElement("card-div");
-        card.classList.add("card");
+        const card = document.createElement("card-div"); // Creating a div for each card.
+        card.classList.add("card"); // Adding card class to style.
+        gameBoard.appendChild(card); // Append card to the game board.
         row.push(card); // Adding card to the row.
     }
     cardGrid.push(row); // Adding row to the Game Card 2D Grid.
 }
 
-    // References for Form and Game Elements is Here!
-    const game = document.getElementById("memoryGame");
-    const form = document.getElementById("registration");
-    // References for Form and Game Elements Ends Here.
+// References for Form and Game Elements is Here!
+const form = document.getElementsByClassName("login");
+const game = document.getElementById("memoryGameBoard");
+// References for Form and Game Elements Ends Here.
 
-    // Event Listener for Form Submission Starts!
+// Event Listener for Form Submission Starts!
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault(); // stops page from refreshing when submit button is clicked.
-        form.style.display = "none"; // Hiding the form after submit button click.
-        game.style.display = "block"; // Reveal the game
-    });
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); // stops page from refreshing when submit button is clicked.
+    form.style.display = "none"; // Hiding the form after submit button click.
+    game.style.display = "block"; // Reveal the game
+});
 
-    // Event Listener for Form Submission Ends.
+// Event Listener for Form Submission Ends.
 // Memory Card Game Ends.
