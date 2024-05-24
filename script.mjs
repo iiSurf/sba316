2059692c6c8fda9f491188a02b993e8db9cad1d3
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-23 20:09:43 -0700

    coming along. Initializing events and card rows and columns

--- script.mjs
+++ script.mjs
@@ -1,4 +1,33 @@
 let paragraph = document.getElementById(`forgotUserPass`);
 paragraph.style.color = "white";
 
-// const cardContainer =
+// Memory Card Game Starts!
+const numberOfRows = 5;
+const numberOfColumns = 5;
+const cardGrid = [];
+
+for (let i = 0; i < numberOfRows; i++) {
+    const row = []; // Initializing an empty row.
+    for (let j = 0; j < numberOfColumns; j++) {
+        const card = document.createElement("card-div");
+        card.classList.add("card");
+        row.push(card); // Adding card to the row.
+    }
+    cardGrid.push(row); // Adding row to the Game Card 2D Grid.
+}
+
+    // References for Form and Game Elements is Here!
+    const game = document.getElementById("memoryGame");
+    const form = document.getElementById("registration");
+    // References for Form and Game Elements Ends Here.
+
+    // Event Listener for Form Submission Starts!
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        form.style.display = "none"; // Hiding the form after submit button click.
+        game.style.display = "block"; // Reveal the game
+    });
+
+    // Event Listener for Form Submission Ends.
+// Memory Card Game Ends.
