d0eb55e55dd1e975c7c3812137e49a7ada2eb18b
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-24 21:16:18 -0700

    reformatted divs and added comments

--- script.mjs
+++ script.mjs
@@ -1,5 +1,5 @@
 let paragraph = document.getElementById(`forgotUserPass`);
-paragraph.style.color = "white";
+// paragraph.style.color = "white";
 
 // Memory Card Game Starts!
 const numberOfRows = 5;
@@ -8,29 +8,31 @@ const cardGrid = [];
 
 const gameBoard = document.querySelector("memoryGameBoard"); // Container for the cards.
 
-for (let i = 0; i < numberOfRows; i++) {
+for (let i = 0; i < numberOfRows; i++) { // for loop for rows
     const row = []; // Initializing an empty row.
-    for (let j = 0; j < numberOfColumns; j++) {
-        const card = document.createElement("card-div"); // Creating a div for each card.
+    for (let j = 0; j < numberOfColumns; j++) { // for loop for columns. Apparently called cells as well?
+        const card = document.createElement("div"); // Creating a div for each card. ("card-div")
+        console.log(`Add cell ${j} to row ${i}`);
         card.classList.add("card"); // Adding card class to style.
         gameBoard.appendChild(card); // Append card to the game board.
         row.push(card); // Adding card to the row.
     }
-    cardGrid.push(row); // Adding row to the Game Card 2D Grid.
+    cardGrid.push(row); // Adding full row of cards to the row.
 }
 
 // References for Form and Game Elements is Here!
-const form = document.getElementsByClassName("login");
+const form = document.getElementById("login");
 const game = document.getElementById("memoryGameBoard");
 // References for Form and Game Elements Ends Here.
 
 // Event Listener for Form Submission Starts!
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault(); // stops page from refreshing when submit button is clicked.
-    form.style.display = "none"; // Hiding the form after submit button click.
-    game.style.display = "block"; // Reveal the game
-});
+
+form.addEventListener('submit', (event) => {
+        form.remove()
+        event.preventDefault()
+        playGame()
+    })
 
 // Event Listener for Form Submission Ends.
 // Memory Card Game Ends.
