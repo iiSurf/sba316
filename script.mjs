8ab2b5b7d382724efbd225d6faa65d7db9307eb0
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-24 23:09:48 -0700

    reformatted divs and added comments

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
-let paragraph = document.getElementById(`forgotUserPass`);
+// let paragraph = document.getElementById(`forgotUserPass`);
 // paragraph.style.color = "white";
 
 // Memory Card Game Starts!
@@ -6,33 +6,38 @@ const numberOfRows = 5;
 const numberOfColumns = 5;
 const cardGrid = [];
 
-const gameBoard = document.querySelector("memoryGameBoard"); // Container for the cards.
+const gameBoard = document.getElementById("memoryGameBoard"); // Container for the cards.
 
 for (let i = 0; i < numberOfRows; i++) { // for loop for rows
-    const row = []; // Initializing an empty row.
+    const row = document.createElement(`div`);
+    row.classList.add(`card-row`);
     for (let j = 0; j < numberOfColumns; j++) { // for loop for columns. Apparently called cells as well?
         const card = document.createElement("div"); // Creating a div for each card. ("card-div")
-        console.log(`Add cell ${j} to row ${i}`);
+        // console.log(`Add cell ${j} to row ${i}`);
         card.classList.add("card"); // Adding card class to style.
-        gameBoard.appendChild(card); // Append card to the game board.
-        row.push(card); // Adding card to the row.
+        // gameBoard.appendChild(card); // Append card to the game board.
+        row.appendChild(card);
+        // row.push(card); // Adding card to the row.
     }
-    cardGrid.push(row); // Adding full row of cards to the row.
+    // cardGrid.push(row
+    gameBoard.appendChild(row); // Adding full row of cards to the row.
 }
 
 // References for Form and Game Elements is Here!
 const form = document.getElementById("login");
-const game = document.getElementById("memoryGameBoard");
+// const game = document.getElementById("memoryGameBoard");
 // References for Form and Game Elements Ends Here.
 
 // Event Listener for Form Submission Starts!
-
-
 form.addEventListener('submit', (event) => {
-        form.remove()
-        event.preventDefault()
-        playGame()
-    })
+    form.style.display = "none";
+    event.preventDefault();
+    // playGame();
+
+    // form.remove()
+    // event.preventDefault()
+    // playGame()
+})
 
 // Event Listener for Form Submission Ends.
 // Memory Card Game Ends.
